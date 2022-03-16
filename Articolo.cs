3614ebff55b1055521398f9bed8671fba64dff7d
XhoniSheqi <learningwebxhoni@gmail.com>
2022-03-16 09:39:59 +0100

    Punto 10 --> impostazione costruttore classe Articolo come utilizzato da frmMain

--- Articolo.cs
+++ Articolo.cs
@@ -22,6 +22,10 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
         {
             // TODO: (10) impostazione costruttore classe Articolo come utilizzato da frmMain
             // ...
+            this.codice = codice;
+            this.descrizione = descrizione;
+            this.unitaMisura = unitaMisura;
+            this.prezzo = prezzo;
         }
 
         public string Visualizzati()
